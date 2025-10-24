64ff3f2d974ab15794aef9e3ef8137e537b66d51
Vashti Domingo <veeornotovee@gmail.com>
2025-10-24 09:12:36 +0800

    Update messy_code.py

--- messy_code.py
+++ messy_code.py
@@ -1,11 +1,22 @@
-#this program add numbers and print the result but its really messy
+# A simple program that adds two numbers and prints the result
 
-def addnumb(a,b):return a+b
-def main( ):
-  print("This is a simple adder program")
-  a=input("enter first number")
-  b=input("enter second number")
-  res=addnumb(int(a),int(b))
-  print("the sum is :",res)
-main( )#call the function at end
-# KELVIN CODE
+def addNumbers(firstNumber, secondNumber):
+  # Returns the sum of two numbers from user input
+  return firstNumber + secondNumber
+
+def main():
+    # Display the description of the program
+    print("\nThis is a simple adder program.\n")
+    
+    # Enter numbers by user to add using the program
+    firstNumber = input("> Enter first number: ")
+    secondNumber = input("> Enter second number: ")
+    
+    # Call the function to add the two numbers from user input
+    addResult = addNumbers(int(firstNumber), int(secondNumber))
+      
+    # Print the result of the addition of two numbers from user input
+    print(f"\nThe sum of {firstNumber} and {secondNumber} is:", addResult, "\n")
+
+# Call the main function to start the program
+main()
